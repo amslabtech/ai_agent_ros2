b129643a16a66eaa9536f5f1570a47469936b3a8
swatanabe <swatanabe@seqsense.com>
2019-06-06 18:35:48 +0900

    fix setup.py

--- demo/src/travel/setup.py
+++ demo/src/travel/setup.py
@@ -5,7 +5,8 @@ package_name = 'control'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=['object_detection','object_detection.yolo3'],
+    packages=['object_detection','yolo3'],
+    package_dir={'yolo3': 'object_detection/yolo3'},
     py_modules=[
         'demo',
         'demo_yolo',
